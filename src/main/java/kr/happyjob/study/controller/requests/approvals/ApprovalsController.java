fba752f5a0bf0251f71b2bf827e35ca4e3485416
JHyun12864 <hyeon12864@naver.com>
2025-09-23 14:19:28 +0900

    2025 09 23 현지영 / 오후 2:19 / first commit

---
+++ src/main/java/kr/happyjob/study/controller/requests/approvals/ApprovalsController.java
@@ -0,0 +1,9 @@
+package kr.happyjob.study.controller.requests.approvals;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApprovalsController{
+
+
+}//class
