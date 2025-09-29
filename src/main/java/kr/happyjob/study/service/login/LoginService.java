bc6983badba4d2442f2862e0dc3feb2bf9dca673
hyunwoo52014 <hyunwoo52014@gmail.com>
2025-09-29 14:37:09 +0900

    코드 정리 및 오류 수정

--- src/main/java/kr/happyjob/study/service/login/LoginService.java
+++ src/main/java/kr/happyjob/study/service/login/LoginService.java
@@ -29,8 +29,8 @@ public class LoginService {
     //import java.util.List;
     //import java.util.Map;
     //
-    //import org.apache.log4j.LogManager;
-    //import org.apache.log4j.Logger;
+    //import org.apache.logging.log4j.LogManager;
+    //import org.apache.logging.Logger;
     //import org.springframework.beans.factory.annotation.Autowired;
     //import org.springframework.stereotype.Service;
     //
--- src/test/java/kr/happyjob/study/Happyjob3ApplicationTests.java
+++ src/test/java/kr/happyjob/study/Happyjob3ApplicationTests.java
@@ -3,11 +3,12 @@ package kr.happyjob.study;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest(classes = Happyjob3Application.class)
 class Happyjob3ApplicationTests {
 
-	//@Test
+	@Test
 	void contextLoads() {
-	}
+        System.out.println("애플리케이션 컨텍스트가 성공적으로 로드되었습니다.");
+    }
 
 }
